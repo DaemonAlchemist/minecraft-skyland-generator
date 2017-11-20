be9b71dd67c549b67e3818384448ec31fcdc9e8b
Andrea <andrea@wittrock.us>
2017-11-20 00:33:24 -0600

    Begin implementing skyland generator

--- src/index.js
+++ src/index.js
@@ -16,30 +16,37 @@ const _printDebug = () => {throw __debug.join("\n");}
 block = context.getBlock(argv[1]);
 const noise = new Noise(1234);
 const size = argv[2] || 10;
-const scale = 100;
-const magnitude = 10;
-const falloff = 2.0;
 
-const _height = (x, z, octaves, scale, magnitude) => 
+const _height = (x, z, octaves, scale, magnitude, falloff) => 
     (noise.perlin2(x/scale, z/scale) + 1)*magnitude + (octaves > 1
-        ? _height(x, z, octaves - 1, scale / falloff, magnitude / falloff)
+        ? _height(x, z, octaves - 1, scale / falloff, magnitude / falloff, falloff)
         : 0.0
     );
-const height = memoize((x, z) => Math.floor(_height(x, z, 8, 100, 10)));
+
+const r1 = 22255;
+const r2 = 243524;
+const r3 = 234234;
+const offset = 30;
+const topHeight    = memoize((x, z) => Math.floor(_height(  x   ,   z   , 8, 100,  5, 2.0)));
+const bottomHeight = memoize((x, z) => Math.floor(_height(x + r1, z + r1, 8,  5 , 10, 1.5)));
+const wave         = memoize((x, z) => Math.floor(_height(x + r2, z + r2, 3, 100, 10, 2.0)));
+const borderWave   = memoize((x, z) => Math.floor(_height(x + r3, z + r3, 3, 100, 10, 2.0)));
 
 //Add the blocks
-for(x=0; x<size; x++) {
-    for(z=0; z<size; z++) {
-        const X = origin.x + x;
-        const Z = origin.z + z;
-        const max = height(X, Z);
-        const min = Math.min(
-            max,
-            height( X , Z-1) + 1,
-            height(X-1,  Z ) + 1,
-            height( X , Z+1) + 1,
-            height(X+1,  Z ) + 1
-        );
+for(x=-size; x<size; x++) {
+    for(z=-size; z<size; z++) {
+        const X = origin.x + x + borderWave(x, z);
+        const Z = origin.z + z + borderWave(x, z);
+        const distFromCenter = Math.sqrt(x*x + z*z);
+        const m = (size - distFromCenter) / size;
+        const max = (
+            wave(X, Z) + offset + m*topHeight(X, Z) +
+            wave(X, Z-1) + offset + m*topHeight(X, Z-1) +
+            wave(X, Z+1) + offset + m*topHeight(X, Z+1) +
+            wave(X-1, Z) + offset + m*topHeight(X-1, Z) +
+            wave(X+1, Z) + offset + m*topHeight(X+1, Z)
+        ) / 5;
+        const min = wave(X, Z) + offset - m*bottomHeight(X, Z);
         for(y=min; y<=max; y++) {
             sess.setBlock(origin.add(x, y - origin.y, z), block);
         }
